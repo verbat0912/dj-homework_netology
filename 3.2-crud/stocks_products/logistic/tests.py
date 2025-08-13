6bdd9910f8f32ebfdc4896a84bd35aae5b985d9c
Verbat <verbat0912@mail.ru>
2025-08-13 18:31:03 +0300

    Add CI

---
+++ 3.2-crud/stocks_products/logistic/tests.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from rest_framework.test import APIClient
+
+class MyTest(TestCase):
+    def test_ok(self):
+        self.assertTrue(True)
+
+    def test_sample_view(self):
+        url = "/api/v1/test/"
+        client = APIClient()
+        response = client.get(url)
+        self.assertEqual(response.status_code,  200)
